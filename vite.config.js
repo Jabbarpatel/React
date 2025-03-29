f0669e2f6555075e93fb51f88448f49f3f40b692
Jabbarpatel <jabbarpatel1947@gmail.com>
2025-03-29 09:30:31 +0530

    docker init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true
+    },
+    strictPort: true,
+    open: true
+  }
+});
